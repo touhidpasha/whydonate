c19fdf9eeffa7c494e9ac86538570da3a39e4722
touhidpasha <touhidpasha552@gmail.com>
2022-12-02 10:08:38 +0530

    FE-setup is partially done

--- whydonate/src/pages/LoginPage.js
+++ whydonate/src/pages/LoginPage.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
+
 import '../css/LoginPage.css'
 
 function LoginPage() {
     const [details,changeDetails]=useState({"email":"",
 "password":""})
 
+const [regPassword,getRegex]=useState(new RegExp("^[a-zA-Z0-9]{8,16}$"))
+
 const getValue=(event)=>{
     changeDetails({...details,[event.target.name]:event.target.value})
+    if(!regPassword.test(details.password))
+    console.log("invalid pass");
+    // alert("password must be only alphaNumeric")
     console.log(details);
+
+}
+
+const verifyData=()=>{
+    axios.put("URL",{data:details})
+
 }
   return (
     <div>
     <h2>LoginPage</h2>
     <input type={"email"} name="email" value={details.email} onChange={getValue}></input>
     <input type={'password'} name="password" value={details.password} onChange={getValue}></input>
-    <button>login</button>
+    <button onClick={verifyData}>login</button>
     </div>
   )
 }
--- whydonate/src/pages/SearchPage.js
+++ whydonate/src/pages/SearchPage.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { useState,useEffect } from 'react';
 import '../css/SearchPage.css'
+import axios from 'axios';
 
 function SearchPage() {
     const [text,getText]=useState("")
     useEffect(() => {
        console.log(text);
     }, [text]);
+
+    const searchData=()=>{
+        axios.get(URL,{text:text})
+    }
   return (
     <div>
     <h3>SearchSomething </h3>
     <input type={"text"} value={text} onChange={(event)=>getText(event.target.value)} />
-    <button >search</button>
+    <button onClick={{searchData}} >search</button>
     </div>
   )
 }
